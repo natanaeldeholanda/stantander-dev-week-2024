698f7141a85476d5ffc42f65a08db14c567ed639
natanaeldeholanda <natan.holanda@gmail.com>
2024-07-13 17:27:00 -0300

    First Commit da Finalização do Bootcamp Santander.

---
+++ src/main/java/me/dio/StantanderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StantanderDevWeek2024Application.class, args);
+	}
+
+}
